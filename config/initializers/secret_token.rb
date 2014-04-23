2ec7f9e674166cbb05a6dbc7e9800e237a6272c3
Stefan Keil <an.stefan.keil@googlemail.com>
2014-04-23 16:43:40 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PunctualityPlan::Application.config.secret_key_base = 'c0c3711b3fc2cfe7faaf3ea4c2a15e2181ad2abcfa2e8a4777c6277d25b5273e19d4af4e5ce1f11637d720adff3c1bcae972338cd0869b30836aac3d24019a5c'
