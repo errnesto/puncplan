140af374e7eeb07de084c0fa4d3880f53ddc3cf3
anna <anna.muenster@web.de>
2014-07-16 17:01:07 +0200

    bubble chart

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -59,9 +59,70 @@
 				result.sort(function(a,b) { return parseFloat(a.avg) - parseFloat(b.avg) } );
 				result.reverse();
 				// createTableFromResult(result);
-				createDivsFromResult(result);
+				// createDivsFromResult(result);
+				var ctx = $("#myChart").get(0).getContext("2d");
+				var data = transformData(result);
+				var options = getOptions();
+				var myBarChart = new Chart(ctx).Bar(data, options);
 
 			});
+
+			function getOptions(){
+				var o = {
+				    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+				    scaleBeginAtZero : true,
+
+				    //Boolean - Whether grid lines are shown across the chart
+				    scaleShowGridLines : true,
+
+				    //String - Colour of the grid lines
+				    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+				    //Number - Width of the grid lines
+				    scaleGridLineWidth : 1,
+
+				    //Boolean - If there is a stroke on each bar
+				    barShowStroke : true,
+
+				    //Number - Pixel width of the bar stroke
+				    barStrokeWidth : 2,
+
+				    //Number - Spacing between each of the X value sets
+				    barValueSpacing : 5,
+
+				    //Number - Spacing between data sets within X values
+				    barDatasetSpacing : 1,
+
+				    //String - A legend template
+				    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+				};
+
+				return o;
+			}
+
+			function transformData(result){
+				var avgs = [];
+				var labels = [];
+				for(var i = 0; i< result.length;i++){
+					avgs.push(result[i].avg);
+					labels.push(result[i].vehicle_type + " " + result[i].vehicle_number);
+				}
+				var data = {
+						    labels: labels,
+						    datasets: [
+						        {
+						            label: "My First dataset",
+						            fillColor: "rgba(220,220,220,0.5)",
+						            strokeColor: "rgba(220,220,220,0.8)",
+						            highlightFill: "rgba(220,220,220,0.75)",
+						            highlightStroke: "rgba(220,220,220,1)",
+						            data: avgs
+						        }
+						       ]
+						    };
+				return data;
+			}
 
 		});
 	});
