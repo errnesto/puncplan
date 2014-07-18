26f5d0ab0fa2a6548c1913835de9bc4717e62259
Manuel Reich <errnesto@me.com>
2014-07-19 00:37:36 +0200

    delete whenever

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
-
 PunctualityPlan::Application.initialize!
